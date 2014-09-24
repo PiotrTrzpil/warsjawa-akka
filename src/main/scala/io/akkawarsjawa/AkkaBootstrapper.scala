dc233a8a1aba27c50ff833850c56b558b5cc7db4
Piotr Trzpil <piotr.trzpil@expectar.com>
2014-09-24 08:49:50 +0200

    remoting config, fixes

--- src/main/scala/io/akkawarsjawa/AkkaBootstrapper.scala
+++ src/main/scala/io/akkawarsjawa/AkkaBootstrapper.scala
@@ -8,8 +8,6 @@ trait AkkaBootstrapper {
    implicit lazy val system = ActorSystem("akka-warsjawa")
    private implicit val _ = system.dispatcher
 
-   val logAggregator = system.actorOf(Props(classOf[LogAggregatorActor]), "logReceiver")
-   val logReceiver = system.actorOf(Props(classOf[LogReceiverActor], logAggregator), "logAggregator")
-
-
+   val logAggregator = system.actorOf(Props(classOf[LogAggregatorActor]), "logAggregator")
+   val logReceiver = system.actorOf(Props(classOf[LogReceiverActor], logAggregator), "logReceiver")
 }
--- src/main/scala/io/akkawarsjawa/AkkaWarsjawa.scala
+++ src/main/scala/io/akkawarsjawa/AkkaWarsjawa.scala
@@ -1,7 +1,5 @@
 package io.akkawarsjawa
 
-import com.codahale.metrics.MetricRegistry
-
 object AkkaWarsjawa extends App with AkkaBootstrapper with SprayBoostrapper {
 
 }
